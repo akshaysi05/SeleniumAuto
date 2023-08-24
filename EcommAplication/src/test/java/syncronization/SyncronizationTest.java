9c4fc3aad253f7644e63e145cfb0838542e1a1ca
akshaysi05 <akshaysingh8804@gmail.com>
2023-08-24 17:01:44 +0530

    Implicit Wait Test

---
+++ EcommAplication/src/test/java/syncronization/SyncronizationTest.java
@@ -0,0 +1,64 @@
+package syncronization;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SyncronizationTest {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		String veggies[] = { "Cucumber", "Brocolli", "Cauliflower" };
+		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
+		Thread.sleep(2000);
+		addItem(driver, veggies);
+		//Cart Click 
+		driver.findElement(By.xpath("//img[@alt='Cart']")).click();
+		driver.findElement(By.xpath("//button[text()='PROCEED TO CHECKOUT']")).click();
+		driver.findElement(By.cssSelector("input.promoCode")).sendKeys("rahulshettyacademy");
+		driver.findElement(By.cssSelector("button.promoBtn")).click();
+		System.out.println( driver.findElement(By.cssSelector("span.promoInfo")).getText());
+		
+
+	}
+
+	public static void addItem(WebDriver driver, String veggies[]) {
+		int j = 0;
+		List<WebElement> ProdName = driver.findElements(By.cssSelector("h4.product-name"));
+		for (int i = 0; i < ProdName.size(); i++) {
+			String name = ProdName.get(i).getText().split(" ")[0].trim();
+
+			// Check weather name you expect is present in Array or not
+			// Convert Array Into Array List for Easy Search
+
+			List VeggiesList = Arrays.asList(veggies);
+
+			if (VeggiesList.contains(name)) {
+				j++;
+				// Add To CartAfter Matching ProductName
+
+				driver.findElements(By.xpath("//div[@class='product-action']/button")).get(i).click();
+
+				if (j == veggies.length) {
+					break;
+				}
+
+			}
+		}
+	}
+
+}
