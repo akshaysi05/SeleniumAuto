6294a2e0c841812b084f0d73948ffcaaba50b6c7
akshaysi05 <akshaysingh8804@gmail.com>
2023-10-02 16:06:08 +0530

    TestNg Learn

---
+++ TestNgTutorial/src/test/java/testng/BasicTestXml.java
@@ -0,0 +1,16 @@
+package testng;
+
+import org.testng.annotations.Test;
+
+public class BasicTestXml {
+	
+	@Test
+	public void Demo() {
+		System.out.println("Learn TestNg First Test");
+	}
+	@Test
+	public void Demo2() {
+		System.out.println("Learn TestNg Second Test");
+	}
+
+}
